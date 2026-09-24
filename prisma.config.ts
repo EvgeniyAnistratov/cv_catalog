98c5bf382f85cc0d604106786e9e600c9df24833
EvgeniyAnistratov <evgeneanistratov@yandex.ru>
2026-09-24 16:34:40 +0300

    fix: add generating connectionString to prisma.config.ts

--- prisma.config.ts
+++ prisma.config.ts
@@ -1,12 +1,20 @@
 import "dotenv/config";
 import { defineConfig, env } from "prisma/config";
 
+const dbUser = `${process.env.DB_USER}`;
+const dbPassword = `${process.env.DB_PASSWORD}`;
+const dbHost = `${process.env.DB_HOST}`;
+const dbPort = `${process.env.DB_PORT}`;
+const dbName = `${process.env.DB_NAME}`;
+const connectionString = `postgresql://${dbUser}:${dbPassword}@${dbHost}:${dbPort}/${dbName}`;
+
 export default defineConfig({
     schema: "./prisma/schema.prisma",
     migrations: {
         path: "./prisma/migrations",
+        seed: "tsx ./prisma/seed.ts",
     },
     datasource: {
-        url: env("DB_URL"),
+        url: connectionString,
     },
 });
